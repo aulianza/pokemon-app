fc36b20e7db094dc04c28173bbd461d33b8e6edf
Ryan Aulia <ryan.aulia@paper.id>
2021-06-22 20:15:36 +0700

    import initial data point

--- src/pages/PokemonList.js
+++ src/pages/PokemonList.js
@@ -7,9 +7,10 @@ import Loading from "../components/Loading";
 import { useQuery } from "@apollo/client";
 import { GET_POKEMON_LIST } from "../graphql/queries/getPokemonList";
 import getOwnedPokemon from "../graphql/queries/getOwnedPokemon";
+import initialDataPoint from "../graphql/operations/initialDataPoint";
 
 const PokemonList = () => {
-    const [limitState, setLimitState] = useState(9);
+    const [limitState, setLimitState] = useState(initialDataPoint());
 
     const variables = { limit: limitState, offset: 0 };
     const { loading, error, data } = useQuery(GET_POKEMON_LIST, { variables });
@@ -26,12 +27,14 @@ const PokemonList = () => {
     };
 
     const loadMoreHandler = () => {
-        setLimitState(limitState + 3);
+        initialDataPoint('more');
+        setLimitState(initialDataPoint());
         scrollToBottom();
     };
 
     const loadLessHandler = () => {
-        setLimitState(limitState - 3);
+        initialDataPoint('less');
+        setLimitState(initialDataPoint());
     };
 
     if (loading) return <Loading message="Loading..." />;
